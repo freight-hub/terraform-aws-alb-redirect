9db8438ce9dce71f07c76d9af1dae5f00b70f8b0
Daniel Lamando <dan@danopia.net>
2020-10-14 16:00:18 +0200

    Add some ARN outputs for listeners, VPC
    Useful for workflows that want to attach stuff to the ALB more directly.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "this_lb_dns_name" {
   description = "Application Load Balancer fqdn"
   value       = aws_lb.this.dns_name
 }
+
+output "http_listener_arn" {
+  description = "ALB HTTP listener ARN"
+  value       = aws_lb_listener.http.arn
+}
+
+output "https_listener_arn" {
+  description = "ALB HTTPS listener ARN"
+  value       = var.https_enabled ? aws_lb_listener.http.arn : null
+}
+
+output "this_vpc_arn" {
+  description = "Virtual Private Cloud ARN"
+  value       = aws_vpc.this.arn
+}
